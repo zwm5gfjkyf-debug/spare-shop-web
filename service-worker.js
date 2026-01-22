98d1bdbe90abf1483357cb6086cdc2d115f27f4a
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-01-22 23:19:03 +0500

    Add service worker for offline support

--- service-worker.js
+++ service-worker.js
@@ -6,13 +6,33 @@ const FILES_TO_CACHE = [
   "/dashboard.html",
   "/add.html",
   "/sales.html",
-  "/manifest.json"
+  "/manifest.json",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
