522197b3e3b596bbea80ec330605f442da79a4f6
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-02-22 07:57:59 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,25 @@
-const CACHE_NAME = "spare-shop-v1";
+const CACHE_NAME = "spare-shop-v2";
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/dashboard.html",
-  "/add.html",
-  "/sales.html",
-  "/manifest.json",
-  "/icon-192x192.png",
-  "/icon-512x512.png"
-];
-// self.addEventListener("install", () => {});
-// self.addEventListener("fetch", () => {});
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  // 🔥 DO NOT CACHE HTML FILES
+  if (event.request.headers.get("accept").includes("text/html")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
